d5486f5e0df24cbcb23780cc4f5ea231b33f1a3a
Christian Treppo <christian@treppo.org>
2014-06-20 15:19:40 +0200

    Simplify role checking specs

--- spec/rspec/roles/subject_spec.rb
+++ spec/rspec/roles/subject_spec.rb
@@ -7,79 +7,96 @@ module RSpec
       describe '#playing_role?' do
         ROLE_NAME = 'Logger'
 
-        it 'is true if the subject is implementing all instance methods defined in the role' do
-          role = define_role do
-            def log(message); end
-            def set; end
-          end
-          roles_repo = fake_roles_repo(role)
-          given_class = Class.new do
-            def set; end
-            def log(message); end
+        let(:roles_repo) { fake_roles_repo(role) }
+
+        context 'role defines instance methods' do
+          let(:role) do
+            define_role do
+              def log(message); end
+              def set; end
+            end
           end
 
-          subj = described_class.new(given_class, roles_repo)
+          it 'is true if the subject is implementing all' do
+            given_class = Class.new do
+              def set; end
+              def log(message); end
+            end
 
-          expect(subj).to be_playing_role ROLE_NAME
+            subj = described_class.new(given_class, roles_repo)
+
+            expect(subj).to be_playing_role ROLE_NAME
+          end
+
+          it 'is false if the subject is not implementing all' do
+            given_class = Class.new do
+              def log(message); end
+            end
+
+            subj = described_class.new(given_class, roles_repo)
+
+            expect(subj).not_to be_playing_role ROLE_NAME
+          end
         end
 
-        it 'is true if the subject is implementing all class methods defined in the role' do
-          role = define_role do
-            def self.create(opts); end
-          end
-          roles_repo = fake_roles_repo(role)
-          given_class = Class.new do
-            def self.create(opts); end
+        context 'role defines class methods' do
+          let(:role) do
+            role = define_role do
+              def self.create(opts); end
+              def self.check; end
+            end
           end
 
-          subj = described_class.new(given_class, roles_repo)
+          it 'is true if the subject is implementing all' do
+            given_class = Class.new do
+              def self.create(opts); end
+              def self.check; end
+            end
 
-          expect(subj).to be_playing_role ROLE_NAME
+            subj = described_class.new(given_class, roles_repo)
+
+            expect(subj).to be_playing_role ROLE_NAME
+          end
+
+          it 'is false if the subject is not implementing all' do
+            given_class = Class.new do
+              def self.check; end
+            end
+
+            subj = described_class.new(given_class, roles_repo)
+
+            expect(subj).not_to be_playing_role ROLE_NAME
+          end
         end
 
-        it 'is false if the subject is not implementing all instance methods defined in the role' do
-          role = define_role do
-            def log(message); end
-            def set; end
-          end
-          roles_repo = fake_roles_repo(role)
-          given_class = Class.new do
-            def log(message); end
+        context 'role defines the constructor' do
+          let(:role) do
+            define_role do
+              def initialize(message); end
+              def log; end
+            end
           end
 
-          subj = described_class.new(given_class, roles_repo)
+          it 'is true if the subject is implementing it' do
+            given_class = Class.new do
+              def initialize(message); end
+              def log; end
+            end
 
-          expect(subj).not_to be_playing_role ROLE_NAME
-        end
+            subj = described_class.new(given_class, roles_repo)
 
-        it 'is false if the subject is not implementing all class methods defined in the role' do
-          role = define_role do
-            def self.create(opts); end
-            def self.check; end
-          end
-          roles_repo = fake_roles_repo(role)
-          given_class = Class.new do
-            def self.check; end
+            expect(subj).to be_playing_role ROLE_NAME
           end
 
-          subj = described_class.new(given_class, roles_repo)
+          it 'is false if the subject is not implementing it' do
+            given_class = Class.new do
+              def log; end
+            end
 
-          expect(subj).not_to be_playing_role ROLE_NAME
-        end
+            subj = described_class.new(given_class, roles_repo)
 
-        it 'is even false if the subject is not implementing the initialize method defined in the role' do
-          role = define_role do
-            def initialize(message); end
-            def log; end
+            expect(subj).not_to be_playing_role ROLE_NAME
           end
-          roles_repo = fake_roles_repo(role)
-          given_class = Class.new do
-            def log; end
-          end
-
-          subj = described_class.new(given_class, roles_repo)
-
-          expect(subj).not_to be_playing_role ROLE_NAME
         end
 
         def define_role(&block)
