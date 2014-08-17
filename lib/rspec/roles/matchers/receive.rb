8a6449e783ae3fb9600b5e698ecd72916c7d7a60
Christian Treppo <christian@treppo.org>
2014-08-17 14:00:01 +0200

    Stop mutating state of receive matcher

--- lib/rspec/roles/matchers/receive.rb
+++ lib/rspec/roles/matchers/receive.rb
@@ -2,26 +2,30 @@ module RSpec
   module Roles
     module Matchers
       class Receive
-        def initialize(method_name)
+        def initialize(method_name, opts = {})
           @method_name = method_name
-          @arguments = []
-          @return_value = nil
+          @args = opts[:args] || []
+          @ret_val = opts[:ret_val]
         end
 
         def matches?(double)
-          double.add_expectation(@method_name, @arguments)
-          double.add_return_value(@method_name, @return_value) if @return_value
+          double.add_expectation(@method_name, @args)
+          double.add_return_value(@method_name, @ret_val) if @ret_val
           double
         end
 
         def with(*args)
-          @arguments = args
-          self
+          create(args: args, ret_val: @ret_val)
         end
 
-        def and_return(return_value)
-          @return_value = return_value
-          self
+        def and_return(ret_val)
+          create(ret_val: ret_val, args: @args)
+        end
+
+        private
+
+        def create(opts)
+          self.class.new(@method_name, {args: @args, ret_val: @ret_val}.merge(opts))
         end
       end
     end
